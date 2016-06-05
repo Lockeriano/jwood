adc0b60173c26e8ab80a0b4c839e9ac6ef151dab
Patryk Skrzyniarz <patryk.skrzyniarz19@gmail.com>
2016-06-05 23:17:57 +0200

    Finished header & added font awesome

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+ 
+gulp.task('default', function () {
+	return gulp.src('css/main.css')
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('dist'));
+});
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
